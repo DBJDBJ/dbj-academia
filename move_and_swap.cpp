6ee83968bd9e4dda0217e14d008b86b740872adc
dbjdbj <dbj@dbj.org>
2020-02-19 16:59:10 +0100

    at last some clarity

--- move_and_swap.cpp
+++ move_and_swap.cpp
@@ -9,7 +9,7 @@
 // using namespace std;
 ///----------------------------------------------------
 namespace dbj {
-    struct S;
+    struct Mytype;
 
     ///----------------------------------------------------
     extern "C" {
@@ -23,13 +23,22 @@ namespace dbj {
         inline void  free_data(char* ip_) {
             printf("\n%20sfreed ( %p )", "", ip_);
             free(ip_);
-            ip_ = nullptr;
+            // ip_ = nullptr;
         }
     }
     ///----------------------------------------------------
+    template <class _Ty>
+    _NODISCARD constexpr std::remove_reference_t<_Ty>&& 
+    move(_Ty&& _Arg) 
+    noexcept { // forward _Arg as movable
+        return static_cast<std::remove_reference_t<_Ty>&&>(_Arg);
+    }
     template <class _Ty, class>
     void swap(_Ty& _Left, _Ty& _Right) 
-        // noexcept(is_nothrow_move_constructible_v<_Ty>&& is_nothrow_move_assignable_v<_Ty>) 
+        noexcept(
+            std::is_nothrow_move_constructible_v<_Ty> && 
+            std::is_nothrow_move_assignable_v<_Ty>
+        ) 
     {
         _Ty _Tmp = std:: move(_Left);
         _Left = std:: move(_Right);
@@ -64,114 +73,146 @@ namespace dbj {
     //}
     ///----------------------------------------------------
     /// there are better designs
-    /// S is like it is for teaching purposes
-    struct S final 
+    /// Mytype is like it is for teaching purposes
+    struct Mytype final 
     {
+        using value_type = char ;
         // POINT: order here rules the oder of initialization
         // in the constructor initializer list
         // not the oder in the list itself!
-        char * data{};
+        value_type * data{};
         size_t size{};
 
-        S() noexcept  : 
+        Mytype() noexcept  : 
             size(0),            // initialized first
             data(nullptr)       // initialized second
         {
-            printf("\n%5sconstructed by default S() data:nullptr", "");
+            printf("\n%5sconstructed by default Mytype() data:nullptr", "");
         }
 
         // POINT: never forget the ctor initializer list
-        explicit S(const char * str_ ) noexcept :
+        explicit Mytype(const char * str_ ) noexcept :
             data(allocate_data(str_)),
             size(strlen(str_))
         {
-            printf("\n%5sconstructed with S(const char *) data: '%s'", "", data);
+            printf("\n%5sconstructed with Mytype(const char *) data: '%s'", "", data);
         }
 
-        ~S() {
-            printf("\n%5sdestructed ~S() with data: '%s'", "", data);
-            if (data) { free_data(data), data = nullptr; size = 0; }
+        ~Mytype() {
+            printf("\n%5sdestructed ~Mytype() with data: '%s'", "", data);
+            /* if (data) */
+            { free_data(data); /*data = nullptr;*/ size = 0; }
         }
 
         // POINT: it is important
         // to initialize the members, before anything else
-        S(S&& other) noexcept 
+        Mytype(Mytype&& other) noexcept 
             : size(std::move(other.size)),
             data(std::move(other.data))
         {
-            printf("\n%5sconstructed with S( S && S{}) ,  data: '%s'", "", data);
+            printf("\n%5sconstructed with Mytype( Mytype && Mytype{}) ,  data: '%s'", "", data);
             // CRITICAL: this is signal to the destructor 
             // of the other object
             // NOT to attempt freeing the moved from data
             other.data = nullptr;
         }
         /* */
-        S(const S& other_) noexcept
+        Mytype(const Mytype& other_) noexcept
             : size(other_.size),
             data(allocate_data(other_.data))
         {
-            printf("\n%5sconstructed  S(const S& other_), data: '%s'", "", data);
+            printf("\n%5sconstructed  Mytype(const Mytype& other_), data: '%s'", "", data);
         }
         /// POINT: copy assignment operator is
         /// required by the swap's
-        constexpr S& operator=(const S&) = default;
+        constexpr Mytype& operator=(const Mytype&) = default;
 
-        //friend void swap(S* a, S* b) {
-        //    printf("\nswap( S * a, S * b)");
-        //    swap(a->data, b->data);
-        //    swap( a->size, b->size );
-        //}
+        // use iter_swap to clearly show the intent
+        // iter_swap is for swapping the pointers to the type
+        friend void iter_swap(Mytype* a, Mytype* b) {
+           printf("\nswap( Mytype * a, Mytype * b)");
+           using std::swap;
+           swap(a->data, b->data);
+           swap( a->size, b->size );
+        }
 
-        //friend void swap(S& a, S& b) {
-        //    printf("\n swap( S & a, S & b)");
-        //    swap(a.data, b.data);
-        //    swap(a.size, b.size);
-        //}
+        // use swap to clearly show the intent
+        // swap is for swapping the value of the type
+        friend void swap(Mytype& a, Mytype& b) {
+           printf("\n swap( Mytype & a, Mytype & b)");
+           std::swap(a.data, b.data);
+           std::swap(a.size, b.size);
+        }
 
     };
 } // dbj
 
+///----------------------------------------------------
 using namespace dbj;
 ///----------------------------------------------------
-/// value argument
-/// return by value
-S return_s(S s_)
+// follow with debugger
+// see the std::move effect
+auto move_shenanigans()
 {
-    return s_; // moving 
+    Mytype * x = new Mytype("White"); 
+    Mytype * y = new Mytype("Black");
+
+    // pointer move    
+    Mytype * xp_move_target = std::move(x);
+    Mytype * yp_move_target = std::move(y);
+
+    // value move
+    Mytype xv_move_target = std::move(*x);
+    Mytype yv_move_target = std::move(*y);
+
+
+    delete x; delete y;
 }
-auto t1()
+auto udt_value_swapping()
 {
-    S x = return_s(S("ABC"));
-    assert(3 == x.size);
+    Mytype x("White"), y("Black");
+
+   // offer first the generic std::swap( T &, T& )
+  // but than make compiler use your implementation
+  // swap( Mytype &, Mytype &)  
+    using std::swap;
+    swap(x,y);
 }
-auto t2()
+auto udt_pointer_swapping()
 {
-    S x("White"), y("Black");
-    std::swap(x, y);
-    //assert(0xF == x.size);
+    Mytype * xp = new Mytype("White"), * yp = new Mytype("Black");
+  // but than make compiler use your implementation
+  // effectively doing
+  // swap( Mytype & * Mytype & * )  
+    using std::swap;
+    swap( *xp, *yp );
+
+    delete xp, delete yp;
 }
-auto t3()
+/// -------------------------------------------------------------
+auto udt_iterator_swapping()
 {
-    /// POINT: swap implementation assures
+    /// POINT: iter_swap udt implementation 
+    /// assures
     /// xp and yp are not changed
     /// just the values they are pointing to
-    S* xp = new S("Red");
-    S* yp = new S("Green");
+    Mytype* xp = new Mytype("Red");
+    Mytype* yp = new Mytype("Green");
 
-    S* xp_before = xp;
-    S* yp_before = yp;
-
-    std::swap(xp, yp);
-
-    auto same_x = xp_before == xp;
-    auto same_y = yp_before == yp;
+    // superfluous
+    using std::iter_swap ;
+    // using Mytype swap
+    std::iter_swap(xp, yp);
 
     delete xp; delete yp;
 }
 
 ///----------------------------------------------------
 int main(int, char**) {
-    t3();
+    // move_shenanigans();
+   udt_value_swapping();
+   udt_pointer_swapping();
+   udt_iterator_swapping();
     return 42;
  }
 ///----------------------------------------------------
